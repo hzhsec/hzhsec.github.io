0b3df7bdc9776db17c980720cba644b67c854146
huangzonghui <1627988979@huangzonghui.com>
2026-04-07 22:34:34 +0800

    publish 护网面经

--- scripts/start_publish_web.bat
+++ scripts/start_publish_web.bat
@@ -1,5 +1,6 @@
 @echo off
 chcp 65001 > nul
 cd /d "%~dp0.."
-python scripts\publish_web.py
-pause
+start "Obsidian ???" cmd /k "python scripts\publish_web.py"
+timeout /t 2 /nobreak > nul
+start "" "http://10.22.167.164:8765/"
